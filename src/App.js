fbbff1f7bb0c7f9bf7d533d2f7d9b8796bda854b
buseseker <sekerbuse_96@hotmail.com>
2024-05-21 16:41:46 +0300

    HeroPage and navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import './App.css';
-
+import './App.scss';
+import Navbar from './components/navbar/Navbar';
+import HeroPage from './components/hero-page/HeroPage'
 function App() {
-  return <div className='App'>
-  </div>;
+  return (
+    <div>
+      <Navbar />
+      <HeroPage/>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from '../../assets/logo.png';
+
+const Navbar = () => {
+  return (
+    <div className='navbar-container'>
+      <div className='logo'>
+        <img src={logo} alt='modernarc-architecture' />
+      </div>
+      <div className='links'>
+        <ul>
+          <li>
+            <a href='#'>Anasayfa</a>
+          </li>
+          <li>
+            <a href='#'>Hakkımızda</a>
+          </li>
+          <li>
+            <a href='#'>Hizmetler</a>
+          </li>
+          <li>
+            <a href='#'>Portfolyo</a>
+          </li>
+          <li>
+            <a href='#'>Blog</a>
+          </li>
+          <li>
+            <a href='#'>İletişim</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

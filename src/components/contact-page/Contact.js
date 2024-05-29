a0dcb16f906c5587a0775d282b41a7753f674c26
buseseker <sekerbuse_96@hotmail.com>
2024-05-30 02:43:57 +0300

    leaflet map

--- src/components/contact-page/Contact.js
+++ src/components/contact-page/Contact.js
@@ -1,39 +1,47 @@
-import React from 'react'
+import React from 'react';
 import slider1 from '../../assets/slider-1.jpg';
+import Map from './Map.js';
+
 const Contact = () => {
+  const position = [51.505, -0.09];
   return (
     <>
-    <div className='page-title'>
-    <div className='title-image'>
-      <img src={slider1} alt='modern-architecture about-us' />
-      <div className='overlay'></div>
-      <h1>İletişim</h1>
-    </div>
-  </div>
-  <div className='contact-forms'>
-    <div className='contact-form'>
-    <i class="fa-sharp fa-regular fa-envelope"></i>
-    <h3>İletişim Formu</h3>
-    <p>Bilgi için bizimle iletişime geçebilirisiniz.</p>
-    </div>
-    <div className='project-form'>
-    <i class="fa-regular fa-building"></i>
-    <h3>Proje Formu</h3>
-    <p>Projeniz için fiyat teklifi alabilirsiniz.</p>
-    </div>
-    <div className='career-form'>
-    <i class="fa-regular fa-clipboard"></i>
-    <h3>Kariyer Formu</h3>
-    <p>Ekibimize katılmak ister misiniz ?</p>
-    </div>
-  </div>
-  <div className='contact-info'>
-    <div className='contact-text'></div>
-    <div className='google-map'></div>
-  </div>
+      <div className='page-title'>
+        <div className='title-image'>
+          <img src={slider1} alt='modern-architecture about-us' />
+          <div className='overlay'></div>
+          <h1>İletişim</h1>
+        </div>
+      </div>
+      <div className='contact-forms'>
+        <div className='contact-form'>
+          <i class='fa-sharp fa-regular fa-envelope'></i>
+          <h3>İletişim Formu</h3>
+          <p>Bilgi için bizimle iletişime geçebilirisiniz.</p>
+        </div>
+        <div className='project-form'>
+          <i class='fa-regular fa-building'></i>
+          <h3>Proje Formu</h3>
+          <p>Projeniz için fiyat teklifi alabilirsiniz.</p>
+        </div>
+        <div className='career-form'>
+          <i class='fa-regular fa-clipboard'></i>
+          <h3>Kariyer Formu</h3>
+          <p>Ekibimize katılmak ister misiniz ?</p>
+        </div>
+      </div>
+      <div className='contact-info'>
+        <div className='contact-text'>
+          <h5>ModernArc Mimarlık</h5>
+          <h6>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, tempore. İstanbul</h6>
+          <p>Tel: +90 212 369 45 54</p>
+          <p> Fax:+90 212 369 45 54</p>
+          <p>E-mail: info@loremipsum.com.tr</p>
+        </div>
+        <Map />
+      </div>
     </>
-    
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
---
+++ src/components/contact-page/Map.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+// Leaflet'in varsayılan işaretçisinin görünmemesi durumunu düzeltmek için bu import'ları yapın
+import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// Leaflet varsayılan simge ayarları
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
+const Map = () => {
+  const position = [51.505, -0.09]; // Londra merkezli koordinatlar
+
+  return (
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={false}
+        dragging={false}
+        style={{ height: '500px', width: '500px' }}
+      >
+        <TileLayer
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}>
+          <Popup>Standart lokasyon: Londra</Popup>
+        </Marker>
+      </MapContainer>
+  );
+};
+
+export default Map;

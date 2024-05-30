f1319c1a02f3dda479713218c7f111f5d3c7831a
buseugrs <sekerbuse_96@hotmail.com>
2024-05-31 00:50:40 +0300

    home page media queries

--- src/components/home-page/footer/Footer.js
+++ src/components/home-page/footer/Footer.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import ProjectIcon from '../../../assets/project-icon.png'
+import React from "react";
+import ProjectIcon from "../../../assets/project-icon.png";
 
 const Footer = () => {
   return (
-    <div className='footer-container'>
-      <div className='form'>
-        <div className='form-icon'>
-          <img src={ProjectIcon} alt='' width={80} />
+    <div className="footer-container">
+      <div className="form">
+        <div className="form-icon">
+          <img src={ProjectIcon} alt="" width={80} />
         </div>
-        <div className='form-text'>
-          <p>Proje fikirleriniz için buradayız.</p>
-          <p>Sizlerle tanışabilmemiz için bizimle iletişime geçmeniz yeterli. </p>
+        <div className="form-text">
+          <span>Proje fikirleriniz için buradayız.</span>
+          <p>
+            Sizlerle tanışabilmemiz için bizimle iletişime geçmeniz yeterli.{" "}
+          </p>
         </div>
-        <div className='button'>
-          <button className='form-btn'>Proje Formu </button>
+        <div className="button">
+          <button className="form-btn">Proje Formu </button>
         </div>
       </div>
-      <div className='copy-right'>
-        <div className='copy-right-text'></div>
-      ModernArc Mimarlık © 2024 Tüm hakları saklıdır.
+      <div className="copy-right">
+        <p className="copy-right-text">
+          ModernArc Mimarlık © 2024 Tüm hakları saklıdır.
+        </p>
       </div>
     </div>
   );
